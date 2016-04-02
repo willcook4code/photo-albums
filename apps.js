e7445927cac7e803244211eb9b38fa655b0c565c
Will Cook <wellsee83@gmail.com>
2016-04-02 13:54:12 -0500

    Got 6 dogs

--- apps.js
+++ apps.js
@@ -1,9 +1,11 @@
 var topPage = document.querySelector('.topPage');
 var midPage = document.querySelector('.midPage');
+var dogsAlbum = document.getElementById('dogsAlbum');
 
 $('#allDogs').click(function() {
 	console.log('There was a click');
 	topPage.style.display = 'none';
 	midPage.style.display = 'block';
 	$('.dogs').addClass('active');
+	dogsAlbum.style.display = 'block';
 });
